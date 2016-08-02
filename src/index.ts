9daf8d6de70cdd480a6d980211eee6f7d28efbff
Jamie <lastmanout@gmail.com>
2016-08-02 16:32:58 -0700

    moving around some files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-/// <reference path="./src/types/animation.d.ts" />
-/// <reference path="./src/types/player.d.ts" />
-
 import {
   ElementAttributeService,
   OpenCloseAnimationService
-} from './src/services';
+} from './services';
 
 export const STATE_ANIMATION_PROVIDERS = [
   ElementAttributeService,
   OpenCloseAnimationService
 ];
 
-export * from './src/services';
+export * from './services';
  
