c20ce99565e5c6c758260cccf5efcda86a9111da
Jamie <lastmanout@gmail.com>
2017-02-01 23:32:52 -0800

    moving the styles inline

--- src/slide/slide.component.ts
+++ src/slide/slide.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng2-slide',
-  styleUrls: [
-    './slide.styles.css'
-  ],
+  styles: [`
+    :host {
+      display: block;
+      height: auto;
+      margin-bottom: 0;
+      height: auto;
+      width: 100%;
+
+      @media (min-width: 1000px) {
+        min-height: 100vh;
+      }
+
+      &.slide {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100%;
+        overflow-y: scroll;
+        -webkit-overflow-scrolling: touch;
+      }
+    }`],
   template: `<ng-content></ng-content>`
 })
 export class SlideComponent {
